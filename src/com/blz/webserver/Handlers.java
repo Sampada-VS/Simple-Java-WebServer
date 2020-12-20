19e0ea28fa20a352d61903dbe9175abe9ca1f603
Sampada-VS <samssv1@gmail.com>
2020-12-20 22:17:05 +0530

    Added: created simple web server in java

--- src/com/blz/webserver/Handlers.java
+++ src/com/blz/webserver/Handlers.java
@@ -18,15 +18,15 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 public class Handlers {
-	
+
 	public static class RootHandler implements HttpHandler {
 
 		@Override
 		public void handle(HttpExchange exchange) throws IOException {
-			String response="<h1>Server start success if you see this message</h1>"+"<h1>Port: "+
-							SimpleHttpServer.port+"</h1>";
+			String response = "<h1>Server start success if you see this message</h1>" + "<h1>Port: "
+					+ SimpleHttpServer.port + "</h1>";
 			exchange.sendResponseHeaders(200, response.length());
-			OutputStream os=exchange.getResponseBody();
+			OutputStream os = exchange.getResponseBody();
 			os.write(response.getBytes());
 			os.close();
 		}
@@ -37,19 +37,19 @@ public class Handlers {
 
 		@Override
 		public void handle(HttpExchange exchange) throws IOException {
-			 Map<String, Object> parameters = new HashMap<String, Object>();
-			 InputStreamReader isr = new InputStreamReader(exchange.getRequestBody(), "utf-8");
-			 BufferedReader br = new BufferedReader(isr);
-			 String query = br.readLine();
-			 parseQuery(query, parameters);
-			
-			 String response = "";
-			 for (String key : parameters.keySet())
-			          response += key + " = " + parameters.get(key) + "\n";
-			 exchange.sendResponseHeaders(200, response.length());
-			 OutputStream os = exchange.getResponseBody();
-			 os.write(response.toString().getBytes());
-			 os.close();
+			Map<String, Object> parameters = new HashMap<String, Object>();
+			InputStreamReader isr = new InputStreamReader(exchange.getRequestBody(), "utf-8");
+			BufferedReader br = new BufferedReader(isr);
+			String query = br.readLine();
+			parseQuery(query, parameters);
+
+			String response = "";
+			for (String key : parameters.keySet())
+				response += key + " = " + parameters.get(key) + "\n";
+			exchange.sendResponseHeaders(200, response.length());
+			OutputStream os = exchange.getResponseBody();
+			os.write(response.toString().getBytes());
+			os.close();
 		}
 	}
 
@@ -58,18 +58,18 @@ public class Handlers {
 		@Override
 		public void handle(HttpExchange exchange) throws IOException {
 			Map<String, Object> parameters = new HashMap<String, Object>();
-            URI requestedUri = exchange.getRequestURI();
-            String query = requestedUri.getRawQuery();
-            parseQuery(query, parameters);
+			URI requestedUri = exchange.getRequestURI();
+			String query = requestedUri.getRawQuery();
+			parseQuery(query, parameters);
 
-            String response = "";
-            for (String key : parameters.keySet())
-                     response += key + " = " + parameters.get(key) + "\n";
-            exchange.sendResponseHeaders(200, response.length());
-            OutputStream os = exchange.getResponseBody();
-            os.write(response.toString().getBytes());
+			String response = "";
+			for (String key : parameters.keySet())
+				response += key + " = " + parameters.get(key) + "\n";
+			exchange.sendResponseHeaders(200, response.length());
+			OutputStream os = exchange.getResponseBody();
+			os.write(response.toString().getBytes());
 
-            os.close();
+			os.close();
 		}
 	}
 
@@ -78,47 +78,47 @@ public class Handlers {
 		@Override
 		public void handle(HttpExchange exchange) throws IOException {
 			Headers headers = exchange.getRequestHeaders();
-            Set<Map.Entry<String, List<String>>> entries = headers.entrySet();
-            String response = "";
-            for (Map.Entry<String, List<String>> entry : entries)
-                     response += entry.toString() + "\n";
-            exchange.sendResponseHeaders(200, response.length());
-            OutputStream os = exchange.getResponseBody();
-            os.write(response.toString().getBytes());
-            os.close();
+			Set<Map.Entry<String, List<String>>> entries = headers.entrySet();
+			String response = "";
+			for (Map.Entry<String, List<String>> entry : entries)
+				response += entry.toString() + "\n";
+			exchange.sendResponseHeaders(200, response.length());
+			OutputStream os = exchange.getResponseBody();
+			os.write(response.toString().getBytes());
+			os.close();
 		}
 	}
 
-	public static void parseQuery(String query, Map<String, Object> parameters) throws UnsupportedEncodingException{
+	public static void parseQuery(String query, Map<String, Object> parameters) throws UnsupportedEncodingException {
 		if (query != null) {
-		    String pairs[] = query.split("[&]");
-		    for (String pair : pairs) {
-		         String param[] = pair.split("[=]");
-		         String key = null;
-		         String value = null;
-		         if (param.length > 0) {
-		        	 key = URLDecoder.decode(param[0], System.getProperty("file.encoding"));
-		         }
-		
-		         if (param.length > 1) {
-		        	 value = URLDecoder.decode(param[1], System.getProperty("file.encoding"));
-		         }
-			
-				 if (parameters.containsKey(key)) {
-					 Object obj = parameters.get(key);
-					 if (obj instanceof List<?>) {
-						 List<String> values = (List<String>) obj;
-						 values.add(value);
-					 } else if (obj instanceof String) {
-						 List<String> values = new ArrayList<String>();
-						 values.add((String) obj);
-						 values.add(value);
-						 parameters.put(key, values);
-					 }
-		         	 } else {
-		         		 parameters.put(key, value);
-		         	 }
-		    }
+			String pairs[] = query.split("[&]");
+			for (String pair : pairs) {
+				String param[] = pair.split("[=]");
+				String key = null;
+				String value = null;
+				if (param.length > 0) {
+					key = URLDecoder.decode(param[0], System.getProperty("file.encoding"));
+				}
+
+				if (param.length > 1) {
+					value = URLDecoder.decode(param[1], System.getProperty("file.encoding"));
+				}
+
+				if (parameters.containsKey(key)) {
+					Object obj = parameters.get(key);
+					if (obj instanceof List<?>) {
+						List<String> values = (List<String>) obj;
+						values.add(value);
+					} else if (obj instanceof String) {
+						List<String> values = new ArrayList<String>();
+						values.add((String) obj);
+						values.add(value);
+						parameters.put(key, values);
+					}
+				} else {
+					parameters.put(key, value);
+				}
+			}
 		}
 	}
 }
